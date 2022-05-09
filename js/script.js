5bec824f6dd414d71a8f63aa87a23aa62ec86f02
puffcooks <76935191+puffcooks@users.noreply.github.com>
2022-05-09 14:26:41 -0400

    save local storage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-getCurrentDate()
+
 
 function getCurrentDate() { let currentDate = moment().format("dddd, MMMM Do YYYY")
 
@@ -12,7 +12,10 @@ let htmlCurrentDate = document.getElementById('currentDay')
  $(htmlCurrentDate).text(currentDate)
 }
 
-let hour = moment().hour()
+let userInput;
+let hourSpan;
+
+let hour = moment().hour();
 
 console.log(hour)
 
@@ -25,14 +28,46 @@ console.log(hour)
  let threePm = $("15pm")
  let fourPm = $("16pm")
  let fivePm = $("17pm")
+
+
+
+function onPageLoad() {
+
+  var load9 = JSON.parse(localStorage.getItem("09:00 AM"));
+  nineAm.val(load9);
+
+  var load10 = JSON.parse(localStorage.getItem("10:00 AM"))
+  tenAm.val(load10)
+  var load11 = JSON.parse(localStorage.getItem("11:00 AM"))
+  tenAm.val(load11)
+  var load12 = JSON.parse(localStorage.getItem("12:00 PM"))
+  tenAm.val(load12)
+  var load13 = JSON.parse(localStorage.getItem("1:00 PM"))
+  tenAm.val(load13)
+  var load14 = JSON.parse(localStorage.getItem("2:00 PM"))
+  tenAm.val(load14)
+  var load15 = JSON.parse(localStorage.getItem("3:00 PM"))
+  tenAm.val(load15)
+  var load16 = JSON.parse(localStorage.getItem("4:00 PM"))
+  tenAm.val(load16)
+  var load17 = JSON.parse(localStorage.getItem("5:00 PM"))
+  tenAm.val(load17)
+  
+
+
+
+
+}
+
+
+
  
- function background () {
+ function changeBackground () {
       
   $(".form-control").each(function () {
       var timeTest = parseInt($(this).attr("id"));
       hour = parseInt(hour);
-      console.log(timeTest);
-      console.log(hour);
+      
 //      console.log(this);
       if (hour > timeTest) {
           $(this).addClass("past");
@@ -44,7 +79,21 @@ console.log(hour)
   });
 }
 
+$(document).ready(function () {
+getCurrentDate()
+changeBackground()
 
-background()
+
+$(".saveBtn").on("click", function(){
+userInput = $(this).siblings(".form-control").val().trim();
+hourSpan = $(this).siblings("input-group-prepend").text().trim();
+localStorage.setItem(hourSpan, JSON.stringify(userInput));
 
 
+
+})
+
+
+
+
+})
